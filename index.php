a00d7644e82433de544788119dafced102cdf293
cosmonaut48 <149193924+cosmonaut48@users.noreply.github.com>
2023-10-28 21:47:14 +0100

    Uploaded files from drive
    These files (intended to enter at index.php) are code written for a simple web-based RPS game

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>R MARS bc70f970</title>
+</head>
+<body>
+<h1>Hello and welcome to this Rock Paper Scissors game!</h1>
+<p><a href="login.php">Please Log In</a></p>
+<p>Don't believe me? Please, <a href="game.php">try it</a>. You'll be shocked.</p>
+</body>
+</html>
